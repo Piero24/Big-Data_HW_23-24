cef413498995f6683794ed5f6e2b44cb7a1f061c
Andrea Pietrobon <andrea_pietrobon@hotmail.it>
2024-03-23 21:56:02 +0100

    first commit

---
+++ Homework_1/src/G019HW1.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class G019HW1 {
+
+    public static void main(String[] args) throws IOException {
+        //
+        // Add the code here ...
+        //
+    }
+}
