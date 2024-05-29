1f5dcdd6d717be616b105e1eb556c3371cc5873c
Piero24 <andrea_pietrobon@hotmail.it>
2024-05-29 12:58:45 +0200

    Created the folder for HW3

---
+++ Homework_3/src/G016HW2.java
@@ -0,0 +1,37 @@
+// Copyright (C) by Group 016 All Rights Reserved
+//
+// This file is part of the project: Homework 3
+//
+// Written by: Pietrobon Andrea, Friso Giovanni, Agostini Francesco
+// Date: June 2024
+
+// Command for execute the homework from terminal:
+// -XX:ReservedCodeCacheSize=256m -Dspark.master="local[*]" G016HW2 ./Homework_2/Data/TestN15-input.txt 3 5 2
+// -XX:ReservedCodeCacheSize=2048m -Dspark.master="local[*]" G016HW2 ./Homework_2/Data/artificial1M_9_100.csv 10 200 4
+// -XX:ReservedCodeCacheSize=2048m -Dspark.master="local[*]" G016HW2 ./Homework_2/Data/uber-large.csv 3 100 16
+
+import java.util.*;
+import java.io.IOException;
+
+import scala.Tuple2;
+import scala.Tuple3;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+
+
+public class G016HW2 {
+
+    /**
+     * 
+     *
+     * @param args 
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+
+    }
+}
