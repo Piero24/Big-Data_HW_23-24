7ef61d7e33a624f8e0b163f5577d0a94824c72bc
GioFriso <134786115+GioFriso@users.noreply.github.com>
2024-06-07 11:10:27 +0200

    Update G016HW3.java with the wanted input printings

--- Homework_3/src/G016HW3.java
+++ Homework_3/src/G016HW3.java
@@ -42,17 +42,17 @@ import java.util.*;
 
 
 //! Used because in vscode the sparkContext is not recognized
-//! It must be removed before the submission 
+//! It must be removed before the submission
 @SuppressWarnings("deprecation")
 public class G016HW3 {
     // After how many items should we stop?
     // public static final int THRESHOLD = 1000000;
 
     /**
-     * 
      *
-     * @param args 
-     * @throws IOException 
+     *
+     * @param args
+     * @throws IOException
      */
     public static void main(String[] args) throws Exception {
 
@@ -65,8 +65,8 @@ public class G016HW3 {
         // code will crash with an out of memory (because the input keeps accumulating).
 
         SparkConf conf = new SparkConf(true)
-        .setMaster("local[*]") //! remove this line if running on the cluster
-        .setAppName("DistinctExample");
+                .setMaster("local[*]") //! remove this line if running on the cluster
+                .setAppName("DistinctExample");
 
         // Here, with the duration you can control how large to make your batches.
         // Beware that the data generator we are using is very fast, so the suggestion
@@ -91,19 +91,11 @@ public class G016HW3 {
         // INPUT READING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         int n = Integer.parseInt(args[0]);
-        System.out.println("Number of items = " + n);
-
         float phi = Float.parseFloat(args[1]);
-        System.out.println("Frequency threshold = " + phi);
-
         float epsilon = Float.parseFloat(args[2]);
-        System.out.println("Accuracy parameter = " + epsilon);
-
         float delta = Float.parseFloat(args[3]);
-        System.out.println("Confidence parameter = " + delta);
-        
         int portExp = Integer.parseInt(args[4]);
-        System.out.println("Receiving data from port = " + portExp);
+        System.out.println("INPUT PROPERTIES\nn = " + n + " phi = " + phi + " epsilon = " + epsilon + " delta = " + delta + " port = " + portExp);
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // DEFINING THE REQUIRED DATA STRUCTURES TO MAINTAIN THE STATE OF THE STREAM
@@ -121,136 +113,109 @@ public class G016HW3 {
         // CODE TO PROCESS AN UNBOUNDED STREAM OF DATA IN BATCHES
         sc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevels.MEMORY_AND_DISK)
 
-        /*Number of items processed = 1095145
-        Number of distinct items = 43621
-        Largest item = 4294473407
+                // For each batch, do the following.
+                .foreachRDD((batch, time) -> {
+                    if (streamLength[0] < n) {
+                        long batchSize = batch.count();
+                        long remaining = n - streamLength[0];
 
-        Number of items processed = 1065816
-        Number of distinct items = 42493
-        Largest item = 4294473407
+                        if (batchSize > remaining) {
+                            batch = batch.zipWithIndex().filter(t -> t._2 < remaining).map(t -> t._1);
+                            batchSize = remaining;
+                        }
+                        streamLength[0] += batchSize;
 
-        Number of items processed = 1019151
-        Number of distinct items = 40649
-        Largest item = 4294473407*/
+                        // Update histogram
+                        JavaPairRDD<String, Long> batchItemCounts = batch.mapToPair(item -> new Tuple2<>(item, 1L))
+                                .reduceByKey(Long::sum);
 
-        // For each batch, to the following.
-        // BEWARE: the `foreachRDD` method has "at least once semantics", meaning
-        // that the same data might be processed multiple times in case of failure.
-        .foreachRDD((batch, time) -> {
-            if (streamLength[0] < n) {
-                long batchSize = batch.count();
-                long remaining = n - streamLength[0];
+                        // Collect the counts to the driver
+                        Map<String, Long> batchCounts = batchItemCounts.collectAsMap();
+                        synchronized (histogram) {
+                            for (Map.Entry<String, Long> entry : batchCounts.entrySet()) {
+                                Long key = Long.parseLong(entry.getKey()); // Parse the key from String to Long
+                                histogram.put(key, histogram.getOrDefault(key, 0L) + entry.getValue());
+                            }
+                        }
 
-                if (batchSize > remaining) {
-                    batch = batch.zipWithIndex().filter(t -> t._2 < remaining).map(t -> t._1);
-                    batchSize = remaining;
-                }
-                streamLength[0] += batchSize;
+                        // Reservoir sampling
+                        List<String> batchItems = batch.collect(); // Collect batch items to a list
+                        for (int i = 0; i < batchSize; i++) {
+                            long globalIndex = streamLength[0] - batchSize + i; // Index in the entire stream
+                            long batchLong = Long.parseLong(batchItems.get(i));
+                            if (globalIndex < m) {
+                                sample.add(batchLong);
+                            } else {
+                                double probability = (double) m / (globalIndex + 1);
+                                double random = Math.random();
+                                if (random < probability) {
+                                    int randomIndex = (int) Math.floor(Math.random() * m);
+                                    sample.set(randomIndex, batchLong);
+                                }
+                            }
 
-                // Update histogram
-                JavaPairRDD<String, Long> batchItemCounts = batch.mapToPair(item -> new Tuple2<>(item, 1L))
-                        .reduceByKey(Long::sum);
+                            // Sticky sampling
+                            if (stickySample.containsKey(batchLong)) {
+                                stickySample.put(batchLong, stickySample.get(batchLong) + 1);
+                            } else {
+                                double stickyProbability = r / n;
+                                double stickyRandom = Math.random();
+                                if (stickyRandom < stickyProbability) {
+                                    stickySample.put(batchLong, 1L);
+                                }
+                            }
+                        }
+                        /*if (batchSize > 0) {
+                            System.out.println("Batch size at time [" + time + "] is: " + batchSize);
+                        }*/
 
-                // Collect the counts to the driver
-                Map<String, Long> batchCounts = batchItemCounts.collectAsMap();
-                synchronized (histogram) {
-                    for (Map.Entry<String, Long> entry : batchCounts.entrySet()) {
-                        Long key = Long.parseLong(entry.getKey()); // Parse the key from String to Long
-                        histogram.put(key, histogram.getOrDefault(key, 0L) + entry.getValue());
-                    }
-                }
-
-                // Reservoir sampling
-                List<String> batchItems = batch.collect(); // Collect batch items to a list
-                for (int i = 0; i < batchSize; i++) {
-                    long globalIndex = streamLength[0] - batchSize + i; // Index in the entire stream
-                    long batchLong = Long.parseLong(batchItems.get(i));
-                    if (globalIndex < m) {
-                        sample.add(batchLong);
-                    } else {
-                        double probability = (double) m / (globalIndex + 1);
-                        double random = Math.random();
-                        if (random < probability) {
-                            int randomIndex = (int) Math.floor(Math.random() * m);
-                            sample.set(randomIndex, batchLong);
+                        if (streamLength[0] >= n) {
+                            stoppingSemaphore.release();
                         }
                     }
-
-                    // Sticky sampling
-                    if (stickySample.containsKey(batchLong)) {
-                        stickySample.put(batchLong, stickySample.get(batchLong) + 1);
-                    } else {
-                        double stickyProbability = r / n;
-                        double stickyRandom = Math.random();
-                        if (stickyRandom < stickyProbability) {
-                            stickySample.put(batchLong, 1L);
-                        }
-                    }
-                }
-                
-
-                if (batchSize > 0) {
-                    System.out.println("Batch size at time [" + time + "] is: " + batchSize);
-                }
-
-                if (streamLength[0] >= n) {
-                    stoppingSemaphore.release();
-                }
-            }
-        });
+                });
         // MANAGING STREAMING SPARK CONTEXT
-        System.out.println("Starting streaming engine");
         sc.start();
-        System.out.println("Waiting for shutdown condition");
         stoppingSemaphore.acquire();
-        System.out.println("Stopping the streaming engine");
-        
-        // NOTE: You will see some data being processed even after the
-        // shutdown command has been issued: This is because we are asking
-        // to stop "gracefully", meaning that any outstanding work
-        // will be done.
         sc.stop(false, false);
-        
-        System.out.println("Streaming engine stopped");
+
         // COMPUTE AND PRINT FINAL STATISTICS
-        System.out.println("Number of items processed = " + streamLength[0]);
+        System.out.println("EXACT ALGORITHM");
+        System.out.println("Number of items in the data structure = " + histogram.size());
         // Calculate and print frequent items
         long thresholdCount = (long) Math.ceil(phi * streamLength[0]);
-        System.out.println("Threshold count = " + thresholdCount);
-        System.out.println("Frequent items (count >= " + thresholdCount + "):");
 
-        if (histogram.isEmpty()) {
+        /*if (histogram.isEmpty()) {
             System.out.println("No items were counted.");
-        }
-
+        }*/
         for (Map.Entry<Long, Long> entry : histogram.entrySet()) {
-           if (entry.getValue() >= thresholdCount) {
+            if (entry.getValue() >= thresholdCount) {
                 trueFrequent.put(entry.getKey(),entry.getValue());
             }
         }
+
+        /*if (histogram.isEmpty()) {
+            System.out.println("No frequent items found.");
+
+        } else {*/
+        System.out.println("Number of true frequent items = " + trueFrequent.size());
+        //}
 
         long first = -1;
 
         List<Long> keys = new ArrayList<>(trueFrequent.keySet());
         Collections.sort(keys);
-
+        System.out.println("True frequent items:");
         for (Long key : keys) {
             if (key > first) {
                 // print or process the entry
-                System.out.println(key + " -> " + trueFrequent.get(key));
+                System.out.println(key);
             }
         }
+        System.out.println("RESERVOIR SAMPLING");
+        System.out.println("Size m of the sample = " + m);
+        //System.out.println("N Frequent Items Estimated by Reservoir Sampling before removal: " + sample.size());
 
-        if (histogram.isEmpty()) {
-            System.out.println("No frequent items found.");
-            
-        } else {
-            System.out.println("N True Frequent Items: " + trueFrequent.size());
-        }
-
-        System.out.println("M: " + m);
-        System.out.println("N Frequent Items Estimated by Reservoir Sampling before removal: " + sample.size());
-        
 
         // List<List<Long>> groupedLists = groupLongs(sample);
 
@@ -266,24 +231,22 @@ public class G016HW3 {
         sample.addAll(set);
 
         Collections.sort(sample);
-
+        System.out.println("Number of estimated frequent items = " + sample.size());
+        System.out.println("Estimated frequent items: ");
         for (Long item : sample) {
             boolean found = false;
 
             for (long key : trueFrequent.keySet())
                 if (item == key)
                     found = true;
-                    
+
             if (!found)
                 System.out.println(item + " -");
             else
                 System.out.println(item + " +");
         }
 
-        //The estimated frequent items, in increasing order (one item per line).
-        // Next to each item print a "+" if the item is a true freuent one,and "-" otherwise
-        System.out.println("N Frequent Items Estimated by Reservoir Sampling: " + sample.size());
-
+        System.out.println("STICKY SAMPLING");
         // At the end, return all items in stickySample with frequency >= (phi - epsilon) * n
         List<Long> frequentItems = new ArrayList<>();
         for (Map.Entry<Long, Long> entry : stickySample.entrySet()) {
@@ -292,21 +255,18 @@ public class G016HW3 {
             }
         }
 
-        System.out.println("r: " + r);
-        System.out.println("Hash Table size by Sticky Sampling: " + stickySample.size());
-        System.out.println("N Frequent Items Estimated by Sticky Sampling: " + frequentItems.size());
-
-        System.out.println("Sticky Sampling : ");
-        
+        //System.out.println("r: " + r);
+        System.out.println("Number of items in the Hash Table = " + stickySample.size());
+        System.out.println("Number of estimated frequent items = " + frequentItems.size());
         Collections.sort(frequentItems);
-
+        System.out.println("Estimated frequent items: ");
         for (Long item : frequentItems) {
             boolean found = false;
 
             for (long key : trueFrequent.keySet())
                 if (item == key)
                     found = true;
-                    
+
             if (!found)
                 System.out.println(item + " -");
             else
@@ -315,48 +275,99 @@ public class G016HW3 {
 
         sc.close();
     }
-
-
-    public static List<List<Long>> groupLongs(List<Long> longList) {
-        // Mappa per memorizzare i gruppi di long
-        Map<Long, List<Long>> longMap = new HashMap<>();
-        
-        // Popola la mappa
-        for (Long num : longList) {
-            if (longMap.containsKey(num)) {
-                longMap.get(num).add(num);
-            } else {
-                List<Long> newList = new ArrayList<>();
-                newList.add(num);
-                longMap.put(num, newList);
-            }
-        }
-        
-        // Crea una lista di liste dai valori della mappa
-        return new ArrayList<>(longMap.values());
-    }
 }
 
-// 1000000 0.01 0.2 0.2 8888
-/*Frequent items (count >= 10322): 1%
-2784604852: 41418
-434415286: 82555
-1936875793: 82726
-2486806931: 82704
-870070186: 82343
-632507218: 41194
-3620094053: 82464
-3837533304: 83318
-978604355: 41362
-3737777178: 82079
-2162156987: 41194
-2967394975: 82643
-2788970093: 82111
-195773912: 82900
-Frequent items (count >= 86590): 8%
-434415286: 86614
-1936875793: 86694
-2486806931: 86780
-3837533304: 87284
-2967394975: 86716
-195773912: 86906*/
+/*INPUT PROPERTIES
+n = 1000000 phi = 0.07 epsilon = 0.03 delta = 0.1 port = 8886
+24/06/07 11:08:06 ERROR ReceiverTracker: Deregistered receiver for stream 0: Stopped by driver
+EXACT ALGORITHM
+Number of items in the data structure = 199444
+Number of true frequent items = 10
+True frequent items:
+195773912
+339323283
+434415286
+641486445
+819911327
+870070186
+1472610405
+1590293530
+1690049656
+1936875793
+RESERVOIR SAMPLING
+Size m of the sample = 15
+Number of estimated frequent items = 11
+Estimated frequent items:
+339323283 +
+434415286 +
+819911327 +
+870070186 +
+1472610405 +
+1517724620 -
+1590293530 +
+1690049656 +
+1936875793 +
+2045000584 -
+2140972778 -
+STICKY SAMPLING
+Number of items in the Hash Table = 37
+Number of estimated frequent items = 10
+Estimated frequent items:
+195773912 +
+339323283 +
+434415286 +
+641486445 +
+819911327 +
+870070186 +
+1472610405 +
+1590293530 +
+1690049656 +
+1936875793 +
+*/
+
+/*
+INPUT PROPERTIES
+n = 1000000 phi = 0.07 epsilon = 0.03 delta = 0.1 port = 8888
+24/06/07 11:06:10 ERROR ReceiverTracker: Deregistered receiver for stream 0: Stopped by driver
+EXACT ALGORITHM
+Number of items in the data structure = 39886
+Number of true frequent items = 10
+True frequent items:
+195773912
+339323283
+434415286
+641486445
+819911327
+870070186
+1472610405
+1590293530
+1690049656
+1936875793
+RESERVOIR SAMPLING
+Size m of the sample = 15
+Number of estimated frequent items = 9
+Estimated frequent items:
+195773912 +
+434415286 +
+561437974 -
+632507218 -
+957456782 -
+1472610405 +
+1570374797 -
+1630839158 -
+1936875793 +
+STICKY SAMPLING
+Number of items in the Hash Table = 24
+Number of estimated frequent items = 10
+Estimated frequent items:
+195773912 +
+339323283 +
+434415286 +
+641486445 +
+819911327 +
+870070186 +
+1472610405 +
+1590293530 +
+1690049656 +
+1936875793 +
+*/
